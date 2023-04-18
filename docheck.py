1b9d7478849a88b26644d2121671cfd53b016745
xdh2580 <1546741822@qq.com>
2023-04-18 18:28:33 +0800

    微调：增加CTS定位项waiver

--- docheck.py
+++ docheck.py
@@ -16,6 +16,7 @@ waiverList = {
     "android.net.cts.MultinetworkApiTest#testNativeDatagramTransmission": "ipv6",
     "android.net.cts.DnsTest#testDnsWorks": "ipv6",
     "com.google.android.location.gts.gnss.GnssPseudorangeVerificationTest#testPseudoPosition": "weak GNSS signal indoor",
+    "android.location.cts.GnssPseudorangeVerificationTest#testPseudoPosition": "weak GNSS signal indoor",
     "com.google.android.media.gts.WidevineDashPolicyTests#testL3OfflineCannotPersist": "waiver",
 }
 
